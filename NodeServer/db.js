3d8b5e8d73d00ec7853e043896834e9831a4e5a6
Uttam1728 <ushankradadiya1728@gmail.com>
2021-02-07 14:13:05 +0530

    DB config changes

--- NodeServer/db.js
+++ NodeServer/db.js
@@ -1,11 +1,10 @@
 const mongoose = require("mongoose");
 const autoIncrement = require('mongoose-auto-increment');
 mongoose.set('useCreateIndex', true);
-
-mongoose.connect("mongodb://localhost:27017/MyMandalDemoDB", {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  },
+mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useNewUrlParser', true);
+mongoose.connect("mongodb://localhost:27017/MyMandalDemoDB", 
   function (err) {
     if (!err) {
       console.log("MongoDB coneection SuccsessFul port 27017...");
