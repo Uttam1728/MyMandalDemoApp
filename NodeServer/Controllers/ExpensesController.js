c867b9b19f4fddc1a7733bb0927b7aa412d728d1
Uttam1728 <ushankradadiya1728@gmail.com>
2021-02-07 14:12:26 +0530

    Update and delete methods updated

--- NodeServer/Controllers/ExpensesController.js
+++ NodeServer/Controllers/ExpensesController.js
@@ -1,5 +1,8 @@
 const Expence = require("../Models/Expense");
 const express = require("express");
+const {
+  isValidObjectId
+} = require("mongoose");
 
 var router = express.Router();
 
@@ -13,6 +16,23 @@ router.get('/', (req, res) => {
     }
   });
 });
+
+
+router.get('/:id', (req, res) => {
+  if (!isValidObjectId(req.params.id)) {
+    res.status(400).send("In Valid Object Id");
+  }
+
+  Expence.find(yreq.params.id, (err, docs) => {
+    if (!err) {
+      res.send(docs);
+    } else {
+      console.log('Error retriving in data', JSON.stringify(err, undefined, 2));
+    }
+  });
+});
+
+
 /*
 Sample Data
         "reason" : 'test reason',
@@ -37,6 +57,7 @@ router.post('/', (req, res) => {
 
   expence.save((err, docs) => {
     if (!err) {
+      //console.log(docs);
       return res.send(docs);
     } else {
       return res.send(JSON.stringify(err, undefined, 2));
@@ -44,4 +65,41 @@ router.post('/', (req, res) => {
   });
 });
 
+
+router.put('/:id', (req, res) => {
+  if (!isValidObjectId(req.params.id)) {
+    res.status(400).send("In Valid Object Id");
+  }
+  var expence = {
+    reason: req.body.reason,
+    cost: req.body.cost,
+    lastUpdatedDate: Date.now(),
+    lastUpdatedBy: req.body.createdBy,
+  };
+
+  Expence.findByIdAndUpdate(req.params.id,
+    expence,
+    { new: true },
+    (err, data) => {
+      if (!err) {
+        res.send(data);
+      } else {
+        console.log('Error saving data', JSON.stringify(err, undefined, 2));
+      }
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  if (!isValidObjectId(req.params.id)) {
+    res.status(400).send("In Valid Object Id");
+  }
+
+  Expence.findByIdAndDelete(req.params.id, (err, docs) => {
+    if (!err) {
+      res.send(docs);
+    } else {
+      console.log('Error saving data', JSON.stringify(err, undefined, 2));
+    }
+  });
+});
 module.exports = router;
