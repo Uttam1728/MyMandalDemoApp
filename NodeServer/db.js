7b6feba8928cd71a0d772e638fe0a70bdb87a269
Uttam1728 <ushankradadiya1728@gmail.com>
2021-02-16 20:36:53 +0530

    Config added

--- NodeServer/db.js
+++ NodeServer/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const autoIncrement = require('mongoose-auto-increment');
+
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useUnifiedTopology', true);
 mongoose.set('useNewUrlParser', true);
-mongoose.connect("mongodb://localhost:27017/MyMandalDemoDB", 
+
+mongoose.connect(process.env.MONGODB_URI, 
   function (err) {
     if (!err) {
       console.log("MongoDB coneection SuccsessFul port 27017...");
